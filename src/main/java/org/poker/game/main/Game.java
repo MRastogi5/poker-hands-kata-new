df55eb5becf5750e61b71878e5bcfda8ac82b3f4
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-03-14 19:27:30 +0000

    clean : refactored and cleanup

--- src/main/java/org/poker/game/main/Game.java
+++ src/main/java/org/poker/game/main/Game.java
@@ -11,175 +11,194 @@ public class Game {
 
     Player player1;
     Player player2;
+
     //Poker game will have two player- Black, White
     public static void main(String[] args) {
-           System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+        //Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH
+        List<Card> cards1 = new ArrayList<>();
+        cards1.add(new Card(Rank.TWO, Suit.Heart));
+        cards1.add(new Card(Rank.TWO, Suit.Diamond));
+        cards1.add(new Card(Rank.FIVE, Suit.Spade));
+        cards1.add(new Card(Rank.NINE, Suit.Club));
+        cards1.add(new Card(Rank.KING, Suit.Diamond));
+        Player firstPlayer = new Player(cards1);
 
         List<Card> cards2 = new ArrayList<>();
-        cards2.add(new Card(Rank.QUEEN, Suit.Heart));
-        cards2.add(new Card(Rank.ACE, Suit.Club));
-        cards2.add(new Card(Rank.KING, Suit.Spade));
-        cards2.add(new Card(Rank.THREE, Suit.Diamond));
-        cards2.add(new Card(Rank.TEN, Suit.Club));
+        cards2.add(new Card(Rank.TWO, Suit.Spade));
+        cards2.add(new Card(Rank.ACE, Suit.Diamond));
+        cards2.add(new Card(Rank.FIVE, Suit.Spade));
+        cards2.add(new Card(Rank.NINE, Suit.Diamond));
+        cards2.add(new Card(Rank.KING, Suit.Club));
 
         Player secondPlayer = new Player(cards2);
-        secondPlayer.getSortedCards();
-        /*List<Card> sortedCards = secondPlayer.getSortedCards();
-        System.out.println("sortedCards : "+ sortedCards);*/
+        Game game = new Game();
+
+        System.out.println("Winning player : " + game.getWinningPokerHands(firstPlayer, secondPlayer));
     }
 
     public void verifyPokerHands(Player firstPlayer, Player secondPlayer) {
-        List<Card> player1Cards= firstPlayer.getCards();
-        List<Card> player2Cards= secondPlayer.getCards();
+        List<Card> player1Cards = firstPlayer.getCards();
+        List<Card> player2Cards = secondPlayer.getCards();
         verifyCards(player1Cards);
         verifyCards(player2Cards);
     }
 
     private void verifyCards(List<Card> cards) {
-        for(Card card : cards){
-            if(!Arrays.asList(Rank.values()).contains(card.getRank())){
+        for (Card card : cards) {
+            if (!Arrays.asList(Rank.values()).contains(card.getRank())) {
                 throw new IllegalArgumentException("Invalid Values");
             }
-            if(!Arrays.asList(Suit.values()).contains(card.getSuit())){
+            if (!Arrays.asList(Suit.values()).contains(card.getSuit())) {
                 throw new IllegalArgumentException("Invalid Values");
             }
         }
-
     }
-
 
     public String getWinningPokerHands(Player firstPlayer, Player secondPlayer) {
         //Sort cards - Done
         //get highest card in player cards - Done
         //check sorted cards for Straight flush, Four of a kind, Full House, Flush, Straight, Three of a Kind,
         //Two Pairs, Pair and set their flags and //setHighCard();
+        //List<Card> sortedCards = firstPlayer.getSortedCards();
+        Map<String, Boolean> firstPlayerCombi = checkCombination(firstPlayer.getSortedCards());
+        Map<String, Boolean> secondPlayerCombi = checkCombination(secondPlayer.getSortedCards());
 
-        List<Card> sortedCards = firstPlayer.getSortedCards();
-        checkCombination(firstPlayer);
+        for (String key : firstPlayerCombi.keySet()) {
+            System.out.println(" First key : " + key + " value : " + firstPlayerCombi.get(key));
+            System.out.println("Second key : " + key + " value : " + secondPlayerCombi.get(key));
+            if (firstPlayerCombi.get(key) != secondPlayerCombi.get(key)) {
+                String result = firstPlayerCombi.get(key) ? "first wins card" : "second wins card";
+                return result;
+            }
+        }
+        return getWinnerbyHighCard(firstPlayer, secondPlayer);
 
-
-
-
-        return "";
     }
 
-    public void checkCombination(Player firstPlayer) {
+    private static String getWinnerbyHighCard(Player firstPlayer, Player secondPlayer) {
+        if (firstPlayer.getHighestCardRankValue() > secondPlayer.getHighestCardRankValue()) {
+            return "first wins";
+        } else if (firstPlayer.getHighestCardRankValue() < secondPlayer.getHighestCardRankValue()) {
+            return "second wins";
+        } else {
+            return "tie";
+        }
+    }
+
+    public Map<String, Boolean> checkCombination(List<Card> sortedCards) {
         Map<String, Boolean> combinations = new HashMap<>();
-
-        //isStraightflush
-        combinations.put("isStraightflush", isStraightflush(firstPlayer));
-        combinations.put("isFourOfAKind", isFourOfAKind(firstPlayer));
-        combinations.put("isFullHouse", isFullHouse(firstPlayer));
-        combinations.put("isFlush",isFlush(firstPlayer));//isFlush
-        combinations.put("isStraight",isStraight(firstPlayer));
-        combinations.put("hasThreeOfAKind",isThreeOfAKind(firstPlayer));
-        combinations.put("hasTwoPair",hasTwoPairs(firstPlayer) );
-        combinations.put("hasPair",hasPair(firstPlayer) );
+        combinations.put("isStraightflush", isStraightflush(sortedCards));
+        combinations.put("isFourOfAKind", isFourOfAKind(sortedCards));
+        combinations.put("isFullHouse", isFullHouse(sortedCards));
+        combinations.put("isFlush", isFlush(sortedCards));//isFlush
+        combinations.put("isStraight", isStraight(sortedCards));
+        combinations.put("hasThreeOfAKind", isThreeOfAKind(sortedCards));
+        combinations.put("hasTwoPair", hasTwoPairs(sortedCards));
+        combinations.put("hasPair", hasPair(sortedCards));
         //hasHignCard(black,white);
+        return combinations;
     }
 
 
-
-    public Boolean isStraightflush(Player player) {
+    public Boolean isStraightflush(List<Card> cards) {
         //5 cards of the same suit with consecutive values : Q♥ J♥ 10♥ 9♥ 8♥
-        if(isStraight(player) && isFlush(player))
+        if (isStraight(cards) && isFlush(cards))
             return true;
         return false;
     }
-    public Boolean isFourOfAKind(Player player) {
+
+    public Boolean isFourOfAKind(List<Card> cards) {
         //4 cards with the same value : 9♣ 9♠ 9♦ 9♥ J♥
-        List<Card> cards = player.getCards();
+
         int i = 0;
-        if(cards.get(i).getRank()==cards.get(i+1).getRank()
-             && cards.get(i+1).getRank()==cards.get(i+2).getRank()
-                && cards.get(i+2).getRank()==(cards.get(i+3).getRank())){
+        if (cards.get(i).getRank() == cards.get(i + 1).getRank()
+                && cards.get(i + 1).getRank() == cards.get(i + 2).getRank()
+                && cards.get(i + 2).getRank() == (cards.get(i + 3).getRank())) {
             return true;
-        }
-        else if(cards.get(i+1).getRank()==cards.get(i+2).getRank()
-                && cards.get(i+2).getRank()==(cards.get(i+3).getRank())
-                && cards.get(i+3).getRank()==cards.get(i+4).getRank()){
+        } else if (cards.get(i + 1).getRank() == cards.get(i + 2).getRank()
+                && cards.get(i + 2).getRank() == (cards.get(i + 3).getRank())
+                && cards.get(i + 3).getRank() == cards.get(i + 4).getRank()) {
             return true;
         }
         return false;
     }
-    public Boolean isFullHouse(Player player) {
+
+    public Boolean isFullHouse(List<Card> cards) {
         //3 cards of the same value, with the remaining 2 cards forming a pair.  3♣ 3♠ 3♦ 6♣ 6♥  or 8♠ 8♦ 8♥ 7♦ 7♣
-        List<Card> cards = player.getCards();
-        int i=0;
-            if((cards.get(i).getRank()==cards.get(i+1).getRank()
-                    && cards.get(i+1).getRank()==cards.get(i+2).getRank())
-                    && cards.get(i+3).getRank().equals(cards.get(i+4).getRank())){
-                return true;
-            }
-            else if((cards.get(i).getRank().equals(cards.get(i+1).getRank()))&&(cards.get(i+2).getRank()==cards.get(i+3).getRank()
-                    && cards.get(i+3).getRank()==cards.get(i+4).getRank())){
-                return true;
-            }
+        int i = 0;
+        if ((cards.get(i).getRank() == cards.get(i + 1).getRank()
+                && cards.get(i + 1).getRank() == cards.get(i + 2).getRank())
+                && cards.get(i + 3).getRank().equals(cards.get(i + 4).getRank())) {
+            return true;
+        } else if ((cards.get(i).getRank().equals(cards.get(i + 1).getRank())) && (cards.get(i + 2).getRank() == cards.get(i + 3).getRank()
+                && cards.get(i + 3).getRank() == cards.get(i + 4).getRank())) {
+            return true;
+        }
 
 
         return false;
     }
-    public Boolean isFlush(Player player) {
+
+    public Boolean isFlush(List<Card> cards) {
         //Hand contains 5 cards of the same suit
         int count = 0;
-        List<Card> cards = player.getCards();
-        for(int i = 1; i<cards.size();i++){
-            if(cards.get(i).getSuit().equals(cards.get(i-1).getSuit())){
+        for (int i = 1; i < cards.size(); i++) {
+            if (cards.get(i).getSuit().equals(cards.get(i - 1).getSuit())) {
                 count++;
             }
         }
-        return count==4;
+        return count == 4;
     }
-    public Boolean isStraight(Player player) {
+
+    public Boolean isStraight(List<Card> cards) {
         //Hand contains 5 cards with consecutive values.
         int count = 0;
-        List<Card> cards = player.getCards();
-       // cards.get(4).getRank().getCardIntValue()
-        for(int i = 1; i<cards.size();i++){
-            if(cards.get(i).getRank().getCardIntValue()-cards.get(i-1).getRank().getCardIntValue() == 1){
+        for (int i = 1; i < cards.size(); i++) {
+            if (cards.get(i).getRank().getCardIntValue() - cards.get(i - 1).getRank().getCardIntValue() == 1) {
                 count++;
             }
         }
-        return count==4;
+        return count == 4;
     }
-    public boolean isThreeOfAKind(Player player) {
+
+    public boolean isThreeOfAKind(List<Card> cards) {
         //Three of the cards in the hand have the same value
-        List<Card> cards = player.getCards();
-        for(int i = 1; i<cards.size();i++){
-            if(cards.get(i-1).getRank()==cards.get(i).getRank()
-                    && cards.get(i).getRank()==cards.get(i+1).getRank()){
+        for (int i = 1; i < cards.size(); i++) {
+            if (cards.get(i - 1).getRank() == cards.get(i).getRank()
+                    && cards.get(i).getRank() == cards.get(i + 1).getRank()) {
                 return true;
             }
         }
         return false;
     }
-    public boolean  hasTwoPairs(Player firstPlayer) {
-        int count = isPair(firstPlayer);
-        if(count == 2)
+
+    public boolean hasTwoPairs(List<Card> cards) {
+        int count = isPair(cards);
+        if (count == 2)
             return true;
         return false;
     }
-    public boolean hasPair(Player firstPlayer) {
-        int count = isPair(firstPlayer);
-        if(count == 1)
+
+    public boolean hasPair(List<Card> cards) {
+        int count = isPair(cards);
+        if (count == 1)
             return true;
         return false;
     }
-    private int isPair(Player player) {
-        List<Card> cards = player.getCards();
+
+    private int isPair(List<Card> cards) {
         int pair_count = 0;
         System.out.println("isPair : " + cards);
-        for(int i = 1; i<cards.size();i++){
-            if(cards.get(i-1).getRank().equals(cards.get(i).getRank())){
-                pair_count++ ;
+        for (int i = 1; i < cards.size(); i++) {
+            if (cards.get(i - 1).getRank().equals(cards.get(i).getRank())) {
+                pair_count++;
             }
         }
         System.out.println("pair_count " + pair_count);
         return pair_count;
     }
-
-
-
 
 
 }
--- src/main/java/org/poker/game/model/Player.java
+++ src/main/java/org/poker/game/model/Player.java
@@ -1,6 +1,6 @@
 package org.poker.game.model;
 
-import java.util.Arrays;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -9,13 +9,20 @@ public class Player {
     // Each player will have 5 Cards called Hand
 
     private List<Card> cards;
+
+    public Card getHighestCard() {
+        return highestCard;
+    }
+
+    public int getHighestCardRankValue() {
+        return highestCard.rank.getCardIntValue();
+    }
+
     private Card highestCard;
 
     public Player(List<Card> cards) {
         this.cards = cards;
-        System.out.println("original : "+ cards);
-
-
+        //System.out.println("original : "+ cards);
     }
 
     public List<Card> getCards() {
@@ -24,8 +31,8 @@ public class Player {
     public List<Card> getSortedCards() {
         Collections.sort(this.cards);
         highestCard = cards.get(cards.size()-1);
-        System.out.println("sorted : "+cards);
-        System.out.println("highestCard : "+highestCard);
+        //System.out.println("sorted : "+cards);
+        //System.out.println("highestCard : "+highestCard);
         return cards;
     }
 }
